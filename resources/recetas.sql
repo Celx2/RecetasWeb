d56dbafb546766e4a1c4cd1fe3fffe354e4dea87
Celeste Laura <celestelaura287@gmail.com>
2021-05-05 17:57:34 +0200

    Segunda tabla SQL

--- resources/recetas.sql
+++ resources/recetas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-05-2021 a las 17:25:54
+-- Tiempo de generación: 05-05-2021 a las 17:55:30
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.2.34
 
